d69a806c153de4daf206ebac5a95880b99b65883
Safa Doyran <doyransafa@gmail.com>
2023-02-08 20:24:30 +0300

    nodemon ve ejs kuruldu. Statik dosyalar ve ortak kodlar ejs ile birlestirildi.

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const ejs = require('ejs')
+const path = require('path')
 
 const app = express();
 
-app.use(express.static('tmp'))
+app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+
+app.get("/index", (req, res) => {
+    res.render("index");
+});
 app.get("/", (req, res) => {
-    res.send("Vay");
+    res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/add", (req, res) => {
+    res.render("add");
+});
+
+app.get("/photo", (req, res) => {
+    res.render("photo");
 });
 
 const port = 3000;
