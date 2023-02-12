1bab8ad22ea22f4f478198b1695ba35d4d8408ad
Safa Doyran <doyransafa@gmail.com>
2023-02-12 23:21:19 +0300

    Update ve Delete yapilari eklendi.

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require('ejs')
 const path = require('path')
+const methodOverride = require('method-override');
+const fs = require('fs')
+const fileUpload = require('express-fileupload');
 const Photo = require('./models/Photo')
+
 
 const app = express();
 
@@ -10,7 +14,7 @@ const app = express();
 mongoose.set('strictQuery', false);
 mongoose
     .connect("mongodb://127.0.0.1:27017/pcat-test-db")
-    .then(() => console.log("Connected!"));
+    .then(() => console.log("Connected to Photo database!"));
 
 app.set('view engine', 'ejs')
 
@@ -18,6 +22,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(fileUpload());
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 
 
 // Routelar
@@ -26,6 +32,13 @@ app.get("/", async (req, res) => {
     res.render("index", {
         photos
     });
+});
+
+app.get("/photo/:id", async (req, res) => {
+    const photo = await Photo.findById(req.params.id)
+    res.render('photo', {
+        photo 
+    })
 });
 
 app.get("/about", (req, res) => {
@@ -41,10 +54,44 @@ app.get("/photo", (req, res) => {
 });
 
 app.post("/add_photo", async (req, res) => {
-    await Photo.create(req.body)
-    await console.log(req.body, 'Added succesfully!')
-    await res.redirect("/")
+
+    let uploadedImage = req.files.image
+    let uploadPath = __dirname  + '/public/uploads/' + uploadedImage.name
+
+    fs.mkdirSync('public/uploads/', { recursive: true })
+
+    uploadedImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadedImage.name 
+        })
+        res.redirect('/')
+    })
+
 });
+
+app.get("/photo/edit/:id", async (req, res) => {
+    const photo = await Photo.findById(req.params.id)
+    res.render('edit', {
+        photo
+    })
+});
+
+
+app.put('/photo/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id);
+    photo.title = req.body.title
+    photo.description = req.body.description
+    await photo.save()
+
+    res.redirect(`/photo/${req.params.id}`)
+})
+
+app.delete('/photo/:id', async (req, res) => {
+    const photo = Photo.findById(req.params.id)
+    await Photo.deleteOne({_id: req.params.id});
+    res.redirect('/')
+})
 
 
 const port = 3000;
